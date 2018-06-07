dc3400ce37f6468f9dd6c7bb2a94627916ad92a6
Eddie Stamps <estamps@doubledutch.me>
2018-06-07 00:00:20 +0000

    Test Edit

--- mobile/src/modalView.js
+++ mobile/src/modalView.js
@@ -38,7 +38,7 @@ export default class ModalView extends Component {
   renderCurrentStage = (stage) => {
     switch (stage) {
       case "1":
-        return <View><Text>Hello</Text></View>
+        return <View><Text>Test</Text></View>
       default:
         return this.renderInputSection(stage)
     }
